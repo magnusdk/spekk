30ebb2a68368f61216b418672997d4d18a550b3d
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-07-05 14:41:48 +0200

    Remove spekk.process code

--- spekk/process/__init__.py
+++
@@ -1,5 +0,0 @@
-from spekk.process.axis import Axis
-from spekk.process.base import Process
-from spekk.process.transformations import Apply, ForAll, Kernel
-
-__all__ = ["Axis", "Process", "ForAll", "Apply", "Kernel"]
