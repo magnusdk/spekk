bcb70dbfaf3f7ca1da0b069c900cde65bf2ab4f3
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-07-12 17:54:58 +0200

    Move slicing and validation to util

---
+++ spekk/util/__init__.py
@@ -0,0 +1,9 @@
+from spekk.util.slicing import slice_data, slice_spec
+from spekk.util.validation import ValidationError, validate
+
+__all__ = [
+    "slice_data",
+    "slice_spec",
+    "ValidationError",
+    "validate",
+]
