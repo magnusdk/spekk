a4ab4a58aca871ae5c9c55af8b95c374fd17106b
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-07-12 17:55:21 +0200

    Add flatten to spekk.util

--- spekk/util/__init__.py
+++ spekk/util/__init__.py
@@ -1,7 +1,9 @@
+from spekk.util.flatten import flatten
 from spekk.util.slicing import slice_data, slice_spec
 from spekk.util.validation import ValidationError, validate
 
 __all__ = [
+    "flatten",
     "slice_data",
     "slice_spec",
     "ValidationError",
