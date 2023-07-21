bd8aa2718cda98bc0311c67408ae46faf4ec64fe
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-07-21 20:54:12 +0200

    Try using a different build backend
    Also update description to match the one on Github

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="spekk",
     version="1.0.1",
-    description="General speccing library",
+    description="Named dimensions of nested arrays, independent of array implementation (Numpy, JAX, etc)",
     author="Magnus Dalen Kvalevåg",
     author_email="magnus.kvalevag@ntnu.no",
     packages=find_packages(),
