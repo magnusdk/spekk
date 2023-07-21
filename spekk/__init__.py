1e83446cd24cf5df184b96baf5943aa699c816d9
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-07-21 21:13:12 +0200

    Add sub-packages to spekk.__init__.py file

--- spekk/__init__.py
+++ spekk/__init__.py
@@ -1,6 +1,12 @@
+import spekk.transformations as transformations
+import spekk.trees as trees
+import spekk.util as util
 from spekk.spec import Spec
 
 __all__ = [
+    "transformations",
+    "trees",
+    "util",
     "Spec",
 ]
-__version__ = "1.0.1"
+__version__ = "1.0.2"
