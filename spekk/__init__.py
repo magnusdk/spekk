1c2edcc2bc5cb53f91fefb912a7bf40c300e83b1
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2024-12-13 13:28:46 +0000

    Add Dims to spekk.__init__

--- spekk/__init__.py
+++ spekk/__init__.py
@@ -1,11 +1,12 @@
 import spekk.ops as ops
 from spekk.module import Module, field
-from spekk.ops._types import Dim
+from spekk.ops._types import Dim, Dims
 
 __all__ = [
     "ops",
     "Module",
     "field",
     "Dim",
+    "Dims",
 ]
 __version__ = "2.0.0"
