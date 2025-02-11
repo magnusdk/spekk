a1a181cd889d5079d0f01501320b67ecffac7a60
tollef <tollef.jahren@gehealthcare.com>
2025-02-11 10:51:45 +0100

    Add check for UndefinedDim in matmul

--- spekk/ops/linear_algebra_functions.py
+++ spekk/ops/linear_algebra_functions.py
@@ -2,7 +2,7 @@ __all__ = ["matmul", "matrix_transpose", "tensordot", "vecdot"]
 
 
 from spekk.ops._backend import backend
-from spekk.ops._types import Dim, Sequence, Tuple, Union
+from spekk.ops._types import Dim, Sequence, Tuple, Union, UndefinedDim
 from spekk.ops._util import ensure_backend_compatible_data, ensure_broadcastable
 from spekk.ops.array_object import array
 from spekk.ops.exceptions import MismatchedDimensionsError
@@ -69,13 +69,14 @@ def matmul(x1: array, x2: array, /) -> array:
 
     # Both are two-dimensional
     if x1.ndim == 2 and x2.ndim == 2:
-        if x1._dims[1] != x2._dims[0]:
-            raise MismatchedDimensionsError(general_error_message)
+        if (x1._dims[1] != x2._dims[0]):
+            if not (isinstance(x1._dims[1], UndefinedDim) and isinstance(x2._dims[0], UndefinedDim)):
+                raise MismatchedDimensionsError(general_error_message)
         return array(
             backend.matmul(x1._data, x2._data),
             [x1._dims[0], x2._dims[1]],
         )
-
+    
     # x1 is one-dimensional, x2 isn't
     if x1.ndim == 1 and x2.ndim != 1:
         if x1._dims[0] != x2._dims[-2]:
