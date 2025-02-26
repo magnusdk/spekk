55972535ab2437b58751ff678dbc6a7507854505
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2025-02-26 09:18:54 +0000

    Remove deprecated functions (more info below)
    Use array.at[dim] instead of take_along_dim and update_indices_along_dim

--- tests/ops/test_vbeam_extensions.py
+++ tests/ops/test_vbeam_extensions.py
@@ -1,72 +1,11 @@
-import random
 from typing import Literal
 
 import numpy as np
 from hypothesis import assume, given
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import array_shapes, arrays
-from tqdm import tqdm
 
 import spekk.ops as ops
-
-
-# TODO: Make me into a hypothesis test
-def foo():
-    with ops.backend.temporary_backend("numpy"):
-        if __name__ == "__main__":
-            for _ in tqdm(range(1000)):
-                dims = {name: size + 1 for name, size in zip("abcdefghijk", range(10))}
-
-                # Construct a random data array x that will be indexed
-                x_dims = random.sample(
-                    list(dims.keys()), random.randint(1, len(dims) - 1)
-                )
-                x = ops.array(np.random.randn(*[dims[d] for d in x_dims]), x_dims)
-
-                # Get a random dimension that will be indexed.
-                x_indexing_dim = random.choice(x_dims)
-
-                # Construct a random indices array i. It contains a dimension i_indexing_dim
-                # which is used to index x. The size of i_indexing_dim may be smaller than
-                # x_sample_dim.
-                i_dims = random.sample(
-                    list(dims.keys()), random.randint(1, len(dims) - 1)
-                )
-                if x_indexing_dim in i_dims:
-                    i_dims.remove(x_indexing_dim)
-                # Convert the x_indexing_dim to a potentially smaller i_indexing_dim.
-                i_indexing_dim = x_indexing_dim + "_i"
-                dims[i_indexing_dim] = random.randint(1, dims[x_indexing_dim])
-                i_dims.append(i_indexing_dim)
-                random.shuffle(i_dims)
-
-                i = ops.array(
-                    np.random.randint(
-                        0, dims[x_indexing_dim], tuple(dims[d] for d in i_dims)
-                    ),
-                    i_dims,
-                )
-
-                # Construct a dictionary of the expected dimension sizes.
-                expected_output_dim_sizes = x.dim_sizes | i.dim_sizes
-                # The resulting array will have the indexing dimension of the indices in place
-                # of the indexing dimension of the data.
-                del expected_output_dim_sizes[x_indexing_dim]
-
-                try:
-                    result = ops.take_along_dim(x, i, x_indexing_dim)
-                    assert expected_output_dim_sizes == result.dim_sizes
-                except Exception:
-                    print(f"{x.dims=}")
-                    print(f"{i.dims=}")
-                    print(f"{x_indexing_dim=}")
-                    print(f"{i_indexing_dim=}")
-                    print(f"{expected_output_dim_sizes=}")
-                    try:
-                        print(f"{result.dim_sizes=}")
-                    except Exception:
-                        pass
-                    raise
 
 
 @given(
