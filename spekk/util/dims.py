015579b17e5e84126437d3cb8ad69d373d4c8c9f
tollef <tollef.jahren@gehealthcare.com>
2025-03-03 11:37:12 +0100

    Made obj optional in random_dim_name

--- spekk/util/dims.py
+++ spekk/util/dims.py
@@ -4,7 +4,7 @@ from typing import Optional
 from spekk.ops._types import Dim
 
 
-def random_dim_name(obj, text: Optional[str] = None) -> Dim:
+def random_dim_name(obj: Optional[str] = None, text: Optional[str] = None) -> Dim:
     """Generate a randomized dimension name that is presumably not meant to be read by
     humans. I.e.: it is presumably some temporary dimension created by a function that
     will be reduced over before returning.
@@ -12,4 +12,6 @@ def random_dim_name(obj, text: Optional[str] = None) -> Dim:
     random_id = str(uuid.uuid4())
     if text:
         random_id = f"{text}_{random_id}"
-    return f"{obj.__module__}.{obj.__class__.__name__}_{random_id}"
+    if obj:
+        random_id = f"{obj.__module__}.{obj.__class__.__name__}_{random_id}"
+    return random_id
